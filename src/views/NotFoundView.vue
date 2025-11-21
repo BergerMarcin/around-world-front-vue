406a439fe2c39b1b227d8210ae91a1e7cf02172a
BergerMarcin <marcin.berger@wp.pl>
2025-11-21 15:00:23 +0100

    styling of 404 not found page

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,13 +1,14 @@
 <template>
-  <div class="unknown-page">
+  <div class="not-found-view">
     <h1>404 - Page Not Found</h1>
     <p>The page you are looking for does not exist.</p>
     <RouterLink to="/">Go back to Home</RouterLink>
   </div>
 </template>
 
-<script setup lang="ts">
-</script>
-
 <style scoped>
+.not-found-view {
+  text-align: center;
+  margin-top: 5rem;
+}
 </style>
