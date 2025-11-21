1576de946798a161d17096b332529a4032da67db
BergerMarcin <marcin.berger@wp.pl>
2025-11-21 14:57:13 +0100

    404 not found page

---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="unknown-page">
+    <h1>404 - Page Not Found</h1>
+    <p>The page you are looking for does not exist.</p>
+    <RouterLink to="/">Go back to Home</RouterLink>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+</style>
