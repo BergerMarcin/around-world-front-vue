32756aa38ef309463d289e2817d654999e3a177a
BergerMarcin <marcin.berger@wp.pl>
2025-11-26 13:38:57 +0100

    logos to separate Logos component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,11 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import Logos from '@/components/logos/Logos.vue'
 </script>
 
 <template>
   <header>
-    <div class="logo-container">
-      <img alt="Around World app logo" class="logo" src="@/assets/logo-around-world.svg" height="25" />
-      <img alt="Vue logo" class="logo" src="@/assets/logo-vue.svg" height="15" width="15" />
-      <img alt="Vite logo" class="logo" src="@/assets/logo-vite.svg" height="15" width="15" />
-    </div>
+    <Logos />
 
     <div class="nav-container">
       <nav>
@@ -26,13 +23,6 @@ header {
   display: flex;
   place-items: center;
   justify-content: space-between;
-}
-
-.logo-container {
-  display: flex;
-  place-items: center;
-  gap: 0.25rem;
-  padding-bottom: 0.25rem;
 }
 
 nav {
---
+++ src/components/logos/Logos.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="logo-container">
+    <img alt="Around World app logo" class="logo" src="@/assets/logo-around-world.svg" height="25" />
+    <img alt="Vue logo" class="logo" src="@/assets/logo-vue.svg" height="15" width="15" />
+    <img alt="Vite logo" class="logo" src="@/assets/logo-vite.svg" height="15" width="15" />
+  </div>
+</template>
+
+<style scoped>
+.logo-container {
+  display: flex;
+  place-items: center;
+  gap: 0.25rem;
+  padding-bottom: 0.25rem;
+}
+</style>
