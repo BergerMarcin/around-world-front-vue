97914e8fe14668b1c3972b8ca91b34f0f03291ff
BergerMarcin <marcin.berger@wp.pl>
2025-12-10 21:35:52 +0100

    feat: move logger hook injection to setup body, imporve error msg

--- src/api/utils/error-handler.ts
+++ src/api/utils/error-handler.ts
@@ -1,11 +1,17 @@
 import { AxiosError } from 'axios'
 import { LogLevel, useLogger } from '@/utils/logger'
 
-const { devLog } = useLogger()
-
 export const errorHandler = <ResponseType>(request: Promise<ResponseType>): Promise<ResponseType> => {
+  const { devLog } = useLogger()
+
   return request.catch((error: AxiosError) => {
-    devLog(LogLevel.error, `API request error from endpoint ${error.config?.baseURL}${error.config?.url}\n`, error.message, error)
+    devLog(
+      LogLevel.error,
+      `API request error from endpoint ${error.config?.baseURL}${error.config?.url}\n`,
+      error.message,
+      '\n',
+      error,
+    )
     throw error
   })
 }
