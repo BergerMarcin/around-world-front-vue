c8d2e14582e9df71503af5cb499e0e753d676935
BergerMarcin <marcin.berger@wp.pl>
2026-01-07 12:30:52 +0100

    refactor: remove base.css and integrate styles into main.css; update button styles for consistency with new color variables

--- src/components/button/BaseButton.vue
+++ src/components/button/BaseButton.vue
@@ -18,56 +18,52 @@ const disabled = props.disabled || false
   </button>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
 .base-button {
   padding: 12px 24px;
+  color: var(--color-button-text);
+  font-size: 1rem;
   border: none;
   border-radius: 8px;
-  font-size: 1rem;
   cursor: pointer;
-}
 
-.base-button:not(:disabled):hover {
-  transform: translateX(-1px) translateY(-1px);
-  box-shadow: 4px 4px 8px var(--color-primary-1-lighter);
-}
+  &:not(:disabled):hover {
+    transform: translateX(-1px) translateY(-1px);
+    box-shadow: 4px 4px 8px var(--color-button-shadow);
+  }
 
-.base-button:not(:disabled):active {
-  transform: none;
-  box-shadow: none;
+  &:not(:disabled):active {
+    transform: none;
+    box-shadow: none;
+  }
+
+  &:disabled,
+  &:disabled:hover,
+  &:disabled:active {
+    color: var(--color-button-disabled-text);
+    background: var(--color-button-disabled-bg);
+    cursor: not-allowed;
+  }
 }
 
 .base-button--primary {
-  background: var(--gradient-primary);
-  color: white;
-}
-
-.base-button--primary:hover {
-  background: var(--gradient-primary-hover);
+  background: var(--color-button-primary-bg);
+  &:hover {
+    background: var(--color-button-primary-bg-hover);
+  }
 }
 
 .base-button--secondary {
-  background-color: var(--color-secondary);
-  color: white;
-}
-
-.base-button--secondary:hover {
-  background-color: var(--color-secondary-dark);
+  background: var(--color-button-secondary-bg);
+  &:hover {
+    background: var(--color-button-secondary-bg-hover);
+  }
 }
 
 .base-button--tertiary {
-  background-color: transparent;
-  color: var(--color-tertiary);
-  border: 2px solid var(--color-tertiary);
-}
-
-.base-button--tertiary:hover {
-  background-color: var(--color-tertiary-hover-bg);
-}
-
-.base-button:disabled {
-  background: var(--color-bg-light-gray);
-  color: var(--color-text-regular);
-  cursor: not-allowed;
+  background: var(--color-button-tertiary-bg);
+  &:hover {
+    background: var(--color-button-tertiary-bg-hover);
+  }
 }
 </style>
