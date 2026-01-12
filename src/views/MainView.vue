a3ff095cbc8359057eabf02c8f3f38379b99065a
BergerMarcin <marcin.berger@wp.pl>
2026-01-12 21:46:07 +0100

    refactor: removed unused MainMap.vue for test-purpose

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,10 +1,9 @@
 <script setup lang="ts">
 import Map from '@/components/map/components/Map.vue'
-// import Map from '@/components/map/components/MainMap.vue'
 </script>
 
 <template>
   <main>
-    <Map></Map>
+    <Map />
   </main>
 </template>
